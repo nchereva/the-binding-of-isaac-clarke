a1289cfd91f73bceebf11fb5c146da217c564f08
Nikolay Cherevatenko <nchereva@gmail.com>
2014-11-23 22:36:39 +0300

    Bullet velocity now depends on player velocity.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 	'use strict';
 
 	var _ = require('lodash');
@@ -43,9 +43,9 @@
 		game.physics.p2.enable(player);
 
 		// player.enableBody = true;
-//		player.body.setCircle(35);
+		//		player.body.setCircle(35);
 		player.body.setCollisionGroup(playerCollisionGroup);
-		player.body.collides([enemiesCollisionGroup, bulletCollisionGroup], function () {
+		player.body.collides([enemiesCollisionGroup, bulletCollisionGroup], function() {
 			console.log('collision')
 		}, this);
 		player.body.fixedRotation = true;
@@ -62,40 +62,47 @@
 		bullets.enableBody = true;
 		bullets.physicsBodyType = Phaser.Physics.P2JS;
 
-		_.each(keys, function (key) {
-			key.onDown.add(function () {
+		_.each(keys, function(key) {
+			key.onDown.add(function() {
 
-					//fullscreen toggle
-					if(key.keyCode == Phaser.Keyboard.F) {
-						toggleFullscreen();
-					}
-
-					var bullet;
-
-					if (key.keyCode == Phaser.Keyboard.A) {
-						bullet = bullets.create(player.position.x - 30, player.position.y, 'bullet');
-						bullet.body.velocity.x += -300;
-					} else if (key.keyCode == Phaser.Keyboard.D) {
-						bullet = bullets.create(player.position.x + 30, player.position.y, 'bullet');
-						bullet.body.velocity.x += 300;
-					} else if (key.keyCode == Phaser.Keyboard.W) {
-						bullet = bullets.create(player.position.x, player.position.y - 30, 'bullet');
-						bullet.body.velocity.y += -300;
-					} else if (key.keyCode == Phaser.Keyboard.S) {
-						bullet = bullets.create(player.position.x, player.position.y + 30, 'bullet');
-						bullet.body.velocity.y += 300;
-					}
-
-					bullet.body.setCircle(10);
-					bullet.body.setCollisionGroup(bulletCollisionGroup);
-					bullet.body.collides([enemiesCollisionGroup], function (bul, en) {
-						bul.sprite.kill();
-						en.sprite.kill();
-						console.log('collision')
-					}, this);
-
+				//fullscreen toggle
+				if (key.keyCode == Phaser.Keyboard.F) {
+					toggleFullscreen();
 				}
-			);
+
+				var bullet,
+					bulletPosition = _.clone(player.position),
+					bulletVelocity = {
+						x: player.body.velocity.x,
+						y: player.body.velocity.y
+					};
+
+				if (key.keyCode == Phaser.Keyboard.A) {
+					bulletPosition.x -= 30;
+					bulletVelocity.x += -300;
+				} else if (key.keyCode == Phaser.Keyboard.D) {
+					bulletPosition.x += 30;
+					bulletVelocity.x += 300;
+				} else if (key.keyCode == Phaser.Keyboard.W) {
+					bulletPosition.y -= 30;
+					bulletVelocity.y += -300;
+				} else if (key.keyCode == Phaser.Keyboard.S) {
+					bulletPosition.y += 30
+					bulletVelocity.y += 300;
+				}
+
+				bullet = bullets.create(bulletPosition.x, bulletPosition.y, 'bullet');
+				bullet.body.velocity.x = bulletVelocity.x;
+				bullet.body.velocity.y = bulletVelocity.y;
+				bullet.body.setCircle(10);
+				bullet.body.setCollisionGroup(bulletCollisionGroup);
+				bullet.body.collides([enemiesCollisionGroup], function(bul, en) {
+					bul.sprite.kill();
+					en.sprite.kill();
+					console.log('collision')
+				}, this);
+
+			});
 		});
 
 		var enemies = game.add.group();
@@ -105,7 +112,7 @@
 		for (var i = 0; i < 20; i++) {
 			var enemy = enemies.create(Math.random() * 800, Math.random() * 600, 'enemy');
 			enemy.body.setCollisionGroup(enemiesCollisionGroup);
-			enemy.body.collides([bulletCollisionGroup, playerCollisionGroup, enemiesCollisionGroup], function (body) {
+			enemy.body.collides([bulletCollisionGroup, playerCollisionGroup, enemiesCollisionGroup], function(body) {
 				enemy.kill();
 				// console.log('collision')
 			});
@@ -124,15 +131,13 @@
 
 		if (cursors.left.isDown) {
 			player.body.velocity.x += -5;
-		}
-		else if (cursors.right.isDown) {
+		} else if (cursors.right.isDown) {
 			player.body.velocity.x += 5;
 		}
 
 		if (cursors.up.isDown) {
 			player.body.velocity.y += -5;
-		}
-		else if (cursors.down.isDown) {
+		} else if (cursors.down.isDown) {
 			player.body.velocity.y += 5;
 		}
 	}
