9575af91e53612fdbb616a7217db90e3095797cb
Dmitry <dzmitry_siver@epam.com>
2014-11-30 20:36:32 +0300

    Room walls created and filled with tile sprite.

--- src/js/main.js
+++ src/js/main.js
@@ -6,22 +6,26 @@
 	var shootAi = require('./ai/shooting');
 
 	var TILE_SIZE = 50,
-		WALL_SIZE = TILE_SIZE * 1.75,
-		TILES_X = 13,
-		TILES_Y = 7,
-		GRID_START = WALL_SIZE + TILE_SIZE / 2;
+		WALL_SIZE = TILE_SIZE * 1.6,
+		TILES_X = 15,
+		TILES_Y = 9,
+		GRID_START = WALL_SIZE + TILE_SIZE / 2,
+		WORLD_WIDTH = TILES_X * TILE_SIZE + 2 * WALL_SIZE,
+		WORLD_HEIGHT = TILES_Y * TILE_SIZE + 2 * WALL_SIZE;
 
 	var stoneMask = [
-		[1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-		[1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-		[1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-		[1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-		[1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-		[1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-		[1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0]
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+		[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+		[0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	];
 
-	var game = new Phaser.Game(TILES_X * TILE_SIZE + 2 * WALL_SIZE, TILES_Y * TILE_SIZE + 2 * WALL_SIZE, Phaser.AUTO, '', {
+	var game = new Phaser.Game(WORLD_WIDTH, WORLD_HEIGHT, Phaser.AUTO, '', {
 		preload: preload,
 		create: create,
 		update: update
@@ -30,7 +34,8 @@
 	var playerCollisionGroup,
 		enemiesCollisionGroup,
 		bulletsCollisionGroup,
-		stonesCollisionGroup;
+		stonesCollisionGroup,
+		wallsCollisionGroup;
 
 	var player,
 		enemies,
@@ -48,13 +53,29 @@
 		fireDelay = 1000,
 		fireDisabled = false;
 
+	var walls,
+		wall_top,
+		wall_bottom,
+		wall_left,
+		wall_right;
+
+	var wall_corner_top_left,
+		wall_corner_top_right,
+		wall_corner_bottom_left,
+		wall_corner_bottom_right;
+
+
 	function preload() {
 
+		game.load.image('wallCorner', 'assets/sprites/wall_corner_1.png');
+		game.load.image('wallPattern', 'assets/sprites/wall_pattern_1.png');
 		game.load.image('player', 'assets/sprites/isaac.png');
 		game.load.image('enemy', 'assets/sprites/enemy.png');
 		game.load.image('bullet', 'assets/sprites/bullet-red.png');
 		game.load.image('stone', 'assets/sprites/stone_1.png');
 		game.load.image('crate', 'assets/sprites/crate_1.png');
+		game.load.image('wall_h', 'assets/sprites/wall_h_1.png');
+		game.load.image('wall_v', 'assets/sprites/wall_v_1.png');
 
 	}
 
@@ -73,15 +94,14 @@
 		enemiesCollisionGroup = game.physics.p2.createCollisionGroup();
 		bulletsCollisionGroup = game.physics.p2.createCollisionGroup();
 		stonesCollisionGroup = game.physics.p2.createCollisionGroup();
-
-		game.physics.p2.updateBoundsCollisionGroup();
+		wallsCollisionGroup = game.physics.p2.createCollisionGroup();
 
 		//creating player
 		player = game.add.sprite(game.world.centerX, game.world.centerY, 'player'); // should be added to group and spawned on grid
-		player.anchor.setTo(0.5, 0.5);
+		//player.anchor.setTo(0.5, 0.5);
 		game.physics.p2.enable(player);
 		player.body.setCollisionGroup(playerCollisionGroup);
-		player.body.collides([enemiesCollisionGroup, bulletsCollisionGroup, stonesCollisionGroup], function () {
+		player.body.collides([enemiesCollisionGroup, bulletsCollisionGroup, stonesCollisionGroup, wallsCollisionGroup], function () {
 			console.log('player collision');
 		}, this);
 		player.body.fixedRotation = true;
@@ -130,10 +150,44 @@
 		stones.enableBody = true;
 		stones.physicsBodyType = Phaser.Physics.P2JS;
 
+		// wall corners
+		wall_corner_top_left = game.add.sprite(0,0, 'wallCorner');
+		wall_corner_top_right = game.add.sprite(WORLD_WIDTH,0, 'wallCorner');
+		wall_corner_bottom_left = game.add.sprite(0,WORLD_HEIGHT, 'wallCorner');
+		wall_corner_bottom_right = game.add.sprite(WORLD_WIDTH,WORLD_HEIGHT, 'wallCorner');
+		wall_corner_top_right.scale.x = -1;
+		wall_corner_bottom_left.scale.y = -1;
+		wall_corner_bottom_right.angle = 180;
+
+		// walls
+		wall_top = game.add.tileSprite(WORLD_WIDTH / 2, WALL_SIZE / 2, TILES_X * TILE_SIZE, WALL_SIZE, 'wallPattern');
+		wall_left = game.add.tileSprite(WALL_SIZE / 2, WORLD_HEIGHT / 2, TILES_Y * TILE_SIZE , WALL_SIZE, 'wallPattern');
+		wall_bottom = game.add.tileSprite(WORLD_WIDTH / 2, WORLD_HEIGHT - WALL_SIZE / 2, TILES_X * TILE_SIZE, WALL_SIZE, 'wallPattern');
+		wall_right = game.add.tileSprite(WORLD_WIDTH - WALL_SIZE / 2, WORLD_HEIGHT / 2, TILES_Y * TILE_SIZE , WALL_SIZE, 'wallPattern');
+
+		wall_left.angle = -90;
+		wall_right.angle = 90;
+		wall_bottom.angle = 180;
+
+		createWall(wall_top);
+		createWall(wall_left);
+		createWall(wall_bottom);
+		createWall(wall_right);
+
 		createLevel(stoneMask, stones);
 
-		createEnemy(enemies, 1, 1);
-		createEnemy(enemies, 11, 5);
+		createEnemy(enemies, 0, 0);
+		createEnemy(enemies, TILES_X-1, TILES_Y-1);
+	}
+
+	function createWall(wallTileSprite){
+		wallTileSprite.enableBody = true;
+		wallTileSprite.physicsBodyType = Phaser.Physics.P2JS;
+		game.physics.p2.enable(wallTileSprite);
+		wallTileSprite.body.setCollisionGroup(wallsCollisionGroup);
+		wallTileSprite.body.fixedRotation = true;
+		wallTileSprite.body.static = true;
+		wallTileSprite.body.collides([playerCollisionGroup, enemiesCollisionGroup, bulletsCollisionGroup, stonesCollisionGroup, wallsCollisionGroup]);
 	}
 
 	function playerFire(direction) {
@@ -187,7 +241,7 @@
 				console.log('collision')
 			}, this);
 
-			bullet.body.collides([stonesCollisionGroup], function (bul, en) {
+			bullet.body.collides([stonesCollisionGroup, wallsCollisionGroup], function (bul, en) {
 				if (bul.sprite.alive) {
 					bul.sprite.kill();
 				}
@@ -204,7 +258,7 @@
 		if (game.scale.isFullScreen) {
 			game.scale.stopFullScreen();
 		} else {
-			game.scale.startFullScreen();
+			game.scale.startFullScreen(false);
 		}
 	}
 
@@ -243,17 +297,17 @@
 	function createStone(group, column, row) {
 		var stone = createAtPoint(group, column, row, 'stone');
 		stone.body.setCollisionGroup(stonesCollisionGroup);
-		stone.body.collides([playerCollisionGroup, enemiesCollisionGroup, bulletsCollisionGroup, stonesCollisionGroup]);
+		stone.body.collides([playerCollisionGroup, enemiesCollisionGroup, bulletsCollisionGroup, stonesCollisionGroup, wallsCollisionGroup]);
 		stone.body.fixedRotation = true;
 		stone.body.static = true;
 	}
 
 	function createEnemy(group, column, row) {
 		var enemy = createAtPoint(group, column, row, 'enemy');
-		enemy._id = _.uniqueId('enemy_')
+		enemy._id = _.uniqueId('enemy_');
 		enemy.body.fixedRotation = true;
 		enemy.body.setCollisionGroup(enemiesCollisionGroup);
-		enemy.body.collides([bulletsCollisionGroup, enemiesCollisionGroup, playerCollisionGroup, stonesCollisionGroup]);
+		enemy.body.collides([bulletsCollisionGroup, enemiesCollisionGroup, playerCollisionGroup, stonesCollisionGroup, wallsCollisionGroup]);
 		enemyAis.push(new shootAi({
 			object: enemy,
 			target: player,
